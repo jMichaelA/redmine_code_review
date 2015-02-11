14eca34adbf32f0d6274b3973553fef60a55f7d4
Jacob Christensen <jacob.h.christensen@gmail.com>
2015-02-10 23:38:28 -0700

    Initial reviews#show view working

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,15 +2,20 @@ class ReviewsController < ApplicationController
   unloadable
 
   layout 'base'
-  before_filter :find_project, :authorize
+  before_filter :find_project # TODO (jchristensen) Add before_filter :authorize when ready
   menu_item :redmine_code_review
 
+  helper RepositoriesHelper
+
   def index
-    @reviews = Review.all
+
   end
 
   def show
-
+    @review = Review.find(params[:review])
+    @changeset = Changeset.find(@review.changeset_id)
+    @repository = Repository.find(@changeset.repository_id)
+    
   end
 
   def new
@@ -29,9 +34,11 @@ class ReviewsController < ApplicationController
   #
   # def reject
   # end
+
 private
   def find_project
-    @project=Project.find(params[:id])
+    @project = Project.find(params[:id])
+
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@
 # See: http://guides.rubyonrails.org/routing.html
 
 match 'projects/:id/reviews', :to => 'reviews#index', :via => 'get'
-match 'projects/:id/reviews', :to => 'reviews#new', :via => 'get'
-match 'projects/:id/reviews', :to => 'reviews#show', :via => 'get'
+match 'projects/:id/reviews/new', :to => 'reviews#new', :via => 'get'
+match 'projects/:id/reviews/:review', :to => 'reviews#show', :via => 'get'
 
 # TODO: (jchristensen)Something like this for the 'Create review' link on the revision page
 #post 'post/:reviews/create', :to => 'reviews#create'
