f710a5d5c85327bb2ea5d3374a504e889c6703d2
Jacob Christensen <jacob.h.christensen@gmail.com>
2015-02-11 01:02:15 -0700

    Other misc stuff... going to bed now

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -36,6 +36,37 @@ class ReviewsController < ApplicationController
   # def reject
   # end
 
+  def diff
+    if params[:format] == 'diff'
+      @diff = @repository.diff(@path, @rev, @rev_to)
+      (show_error_not_found; return) unless @diff
+      filename = "changeset_r#{@rev}"
+      filename << "_r#{@rev_to}" if @rev_to
+      send_data @diff.join, :filename => "#{filename}.diff",
+                :type => 'text/x-patch',
+                :disposition => 'attachment'
+    else
+      @diff_type = params[:type] || User.current.pref[:diff_type] || 'inline'
+      @diff_type = 'inline' unless %w(inline sbs).include?(@diff_type)
+
+      # Save diff type as user preference
+      if User.current.logged? && @diff_type != User.current.pref[:diff_type]
+        User.current.pref[:diff_type] = @diff_type
+        User.current.preference.save
+      end
+      @cache_key = "repositories/diff/#{@repository.id}/" +
+          Digest::MD5.hexdigest("#{@path}-#{@rev}-#{@rev_to}-#{@diff_type}-#{current_language}")
+      unless read_fragment(@cache_key)
+        @diff = @repository.diff(@path, @rev, @rev_to)
+        show_error_not_found unless @diff
+      end
+
+      @changeset = @repository.find_changeset_by_name(@rev)
+      @changeset_to = @rev_to ? @repository.find_changeset_by_name(@rev_to) : nil
+      @diff_format_revisions = @repository.diff_format_revisions(@changeset, @changeset_to)
+    end
+  end
+
 private
   def find_project
     @project = Project.find(params[:id])
