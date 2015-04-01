ef73fc63331a0d6ad95a2b370f79a86cb4cbc131
cloudhunter89 <litereader@gmail.com>
2015-03-31 19:06:57 -0600

    added default columns to review index (frozen attribute)

--- app/models/review_query.rb
+++ app/models/review_query.rb
@@ -6,12 +6,12 @@ class ReviewQuery < Query
   self.available_columns= [
       QueryColumn.new(:id, :sortable => "#{Review.table_name}.id", :default_order => 'desc', :caption => '#', :frozen => true),
       QueryColumn.new(:revision, :sortable => "#{Changeset.table_name}.revision", :groupable => true),
-      QueryColumn.new(:description, :sortable => "#{Changeset.table_name}.comments"),
+      QueryColumn.new(:subject, :sortable => "#{Changeset.table_name}.comments", :inline => true, :frozen => true),
       QueryColumn.new(:requester, :sortable => "#{User.table_name}.name", :groupable => true),
       QueryColumn.new(:project, :sortable => "#{Project.table_name}.name", :groupable => true),
-      QueryColumn.new(:due_date, :sortable => "#{Review.table_name}.due_date"),
-      QueryColumn.new(:priority, :sortable => "#{IssuePriority.table_name}.position", :default_order => 'desc', :groupable => true),
-      QueryColumn.new(:created_on, :sortable => "#{Review.table_name}.created_on", :groupable => true),
+      QueryColumn.new(:priority, :sortable => "#{IssuePriority.table_name}.position", :default_order => 'desc', :groupable => true, :frozen => true),
+      QueryColumn.new(:created_on, :sortable => "#{Review.table_name}.created_on", :groupable => true, :frozen => true),
+      QueryColumn.new(:due_date, :sortable => "#{Review.table_name}.due_date", :frozen => true),
       QueryColumn.new(:updated_on, :sortable => "#{Review.table_name}.updated_on", :groupable => true),
       QueryColumn.new(:closed, :sotrable => "#{Review.table_name}.closed", :groupable => true)
   ]
