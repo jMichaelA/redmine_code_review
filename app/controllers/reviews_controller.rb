db0229ab46034fa3aff48bbe933ac0ab30317e8d
Riley Oakden <roakden3@gmail.com>
2015-04-13 00:54:47 -0600

    Still working on adding approvers, approver list is not being submitted with the form currently

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -86,11 +86,17 @@ class ReviewsController < ApplicationController
     @changeset = Changeset.find(params[:rev])
     @repository = Repository.find(@changeset.repository_id)
     # TODO: update this to use the specified permissions (currently gets all involved with the project)
-    @possible_approvers = Member.find_all_by_project_id(@repository.project_id).uniq
+    @project_members = Member.find_all_by_project_id(@repository.project_id).uniq
     # Use this foreach to strip out potential approvers that don't have appropriate permissions
     #@possible_approvers.each do |approver|
     #  if approver.
     #end
+    @possible_approvers = []
+
+    @project_members.each do |member|
+      @possible_approvers.push(User.find(member.user_id))
+    end
+
     @possible_watchers = Member.find_all_by_project_id(@repository.project_id).uniq
   end
 
